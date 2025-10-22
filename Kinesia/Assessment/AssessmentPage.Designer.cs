7ee89190e3f35685fe77b2c4e1dcc618387d7ca7
Adrianne Villa <137845861+AdrianneVilla@users.noreply.github.com>
2025-10-22 11:17:09 +0800

    Feat(UI): Adding sort and buttons for status of assessment

--- Kinesia/Assessment/AssessmentPage.Designer.cs
+++ Kinesia/Assessment/AssessmentPage.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AssessmentPage));
             label1 = new System.Windows.Forms.Label();
             nameHolder = new System.Windows.Forms.Label();
@@ -44,11 +44,20 @@
             customButton1 = new OrganizationProfile.CustomButton();
             pictureBox1 = new System.Windows.Forms.PictureBox();
             rjTextBox1 = new CustomControls.RJControls.RJTextBox();
+            cbSort = new CustomControls.RJControls.RJComboBox();
+            panelBorder3 = new WindowsFormsApp2.CustomButton.PanelBorder();
+            rjComboBox1 = new CustomControls.RJControls.RJComboBox();
+            customButton2 = new OrganizationProfile.CustomButton();
+            customButton3 = new OrganizationProfile.CustomButton();
+            customButton4 = new OrganizationProfile.CustomButton();
+            customButton5 = new OrganizationProfile.CustomButton();
+            customButton6 = new OrganizationProfile.CustomButton();
             PatientHolder.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridAssessments).BeginInit();
             panelBorder2.SuspendLayout();
             panelBorder1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panelBorder3.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -82,6 +91,7 @@
             PatientHolder.BackColor = System.Drawing.Color.FromArgb(207, 249, 238);
             PatientHolder.BorderRadius = 10;
             PatientHolder.Color = System.Drawing.Color.White;
+            PatientHolder.Controls.Add(panelBorder3);
             PatientHolder.Controls.Add(dataGridAssessments);
             PatientHolder.ForeColor = System.Drawing.Color.Black;
             PatientHolder.Location = new System.Drawing.Point(72, 236);
@@ -105,34 +115,34 @@
             dataGridAssessments.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             dataGridAssessments.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
             dataGridAssessments.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Transparent;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Transparent;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            dataGridAssessments.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            dataGridAssessments.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             dataGridAssessments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridAssessments.GridColor = System.Drawing.Color.White;
-            dataGridAssessments.Location = new System.Drawing.Point(16, 16);
+            dataGridAssessments.Location = new System.Drawing.Point(16, 56);
             dataGridAssessments.Margin = new System.Windows.Forms.Padding(10);
             dataGridAssessments.Name = "dataGridAssessments";
             dataGridAssessments.ReadOnly = true;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Transparent;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Transparent;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            dataGridAssessments.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            dataGridAssessments.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dataGridAssessments.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             dataGridAssessments.ShowCellErrors = false;
             dataGridAssessments.ShowCellToolTips = false;
             dataGridAssessments.ShowEditingIcon = false;
             dataGridAssessments.ShowRowErrors = false;
-            dataGridAssessments.Size = new System.Drawing.Size(1285, 554);
+            dataGridAssessments.Size = new System.Drawing.Size(1285, 514);
             dataGridAssessments.TabIndex = 11;
             dataGridAssessments.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -169,6 +179,7 @@
             panelBorder2.BackColor = System.Drawing.Color.White;
             panelBorder2.BorderRadius = 30;
             panelBorder2.Color = System.Drawing.Color.BurlyWood;
+            panelBorder2.Controls.Add(cbSort);
             panelBorder2.Controls.Add(btnLowerExtremities);
             panelBorder2.Controls.Add(btnUpperExtremities);
             panelBorder2.Controls.Add(btnAll);
@@ -316,6 +327,175 @@
             rjTextBox1.Texts = "";
             rjTextBox1.UnderlinedStyle = false;
             // 
+            // cbSort
+            // 
+            cbSort.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            cbSort.BackColor = System.Drawing.Color.White;
+            cbSort.BorderColor = System.Drawing.Color.Gray;
+            cbSort.BorderSize = 1;
+            cbSort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            cbSort.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            cbSort.ForeColor = System.Drawing.Color.DimGray;
+            cbSort.IconColor = System.Drawing.Color.FromArgb(24, 90, 211);
+            cbSort.Items.AddRange(new object[] { "Default", "Alphabetical (Name)", "Earliest (Date Added)", "Latest (Date Added)" });
+            cbSort.ListBackColor = System.Drawing.Color.White;
+            cbSort.ListTextColor = System.Drawing.Color.Black;
+            cbSort.Location = new System.Drawing.Point(804, 7);
+            cbSort.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            cbSort.MinimumSize = new System.Drawing.Size(233, 35);
+            cbSort.Name = "cbSort";
+            cbSort.Padding = new System.Windows.Forms.Padding(1);
+            cbSort.Size = new System.Drawing.Size(284, 55);
+            cbSort.TabIndex = 15;
+            cbSort.Texts = "Default";
+            // 
+            // panelBorder3
+            // 
+            panelBorder3.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            panelBorder3.BackColor = System.Drawing.Color.Transparent;
+            panelBorder3.BorderRadius = 15;
+            panelBorder3.Color = System.Drawing.Color.BurlyWood;
+            panelBorder3.Controls.Add(customButton6);
+            panelBorder3.Controls.Add(rjComboBox1);
+            panelBorder3.Controls.Add(customButton2);
+            panelBorder3.Controls.Add(customButton3);
+            panelBorder3.Controls.Add(customButton4);
+            panelBorder3.Controls.Add(customButton5);
+            panelBorder3.ForeColor = System.Drawing.Color.Black;
+            panelBorder3.Location = new System.Drawing.Point(707, 9);
+            panelBorder3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            panelBorder3.Name = "panelBorder3";
+            panelBorder3.Size = new System.Drawing.Size(603, 46);
+            panelBorder3.TabIndex = 17;
+            // 
+            // rjComboBox1
+            // 
+            rjComboBox1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            rjComboBox1.BackColor = System.Drawing.Color.White;
+            rjComboBox1.BorderColor = System.Drawing.Color.Gray;
+            rjComboBox1.BorderSize = 1;
+            rjComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            rjComboBox1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            rjComboBox1.ForeColor = System.Drawing.Color.DimGray;
+            rjComboBox1.IconColor = System.Drawing.Color.FromArgb(24, 90, 211);
+            rjComboBox1.Items.AddRange(new object[] { "Default", "Alphabetical (Name)", "Earliest (Date Added)", "Latest (Date Added)" });
+            rjComboBox1.ListBackColor = System.Drawing.Color.White;
+            rjComboBox1.ListTextColor = System.Drawing.Color.Black;
+            rjComboBox1.Location = new System.Drawing.Point(1057, 7);
+            rjComboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            rjComboBox1.MinimumSize = new System.Drawing.Size(233, 35);
+            rjComboBox1.Name = "rjComboBox1";
+            rjComboBox1.Padding = new System.Windows.Forms.Padding(1);
+            rjComboBox1.Size = new System.Drawing.Size(284, 55);
+            rjComboBox1.TabIndex = 15;
+            rjComboBox1.Texts = "Default";
+            // 
+            // customButton2
+            // 
+            customButton2.BackColor = System.Drawing.Color.Gainsboro;
+            customButton2.BackgroundColor = System.Drawing.Color.Gainsboro;
+            customButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            customButton2.BorderRadius = 5;
+            customButton2.BorderSize = 0;
+            customButton2.FlatAppearance.BorderSize = 0;
+            customButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            customButton2.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline);
+            customButton2.ForeColor = System.Drawing.Color.Gray;
+            customButton2.Location = new System.Drawing.Point(273, 12);
+            customButton2.Margin = new System.Windows.Forms.Padding(1);
+            customButton2.Name = "customButton2";
+            customButton2.Size = new System.Drawing.Size(156, 31);
+            customButton2.TabIndex = 12;
+            customButton2.Text = "Finished";
+            customButton2.TextColor = System.Drawing.Color.Gray;
+            customButton2.UseVisualStyleBackColor = false;
+            // 
+            // customButton3
+            // 
+            customButton3.BackColor = System.Drawing.Color.Gainsboro;
+            customButton3.BackgroundColor = System.Drawing.Color.Gainsboro;
+            customButton3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            customButton3.BorderRadius = 5;
+            customButton3.BorderSize = 0;
+            customButton3.FlatAppearance.BorderSize = 0;
+            customButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            customButton3.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline);
+            customButton3.ForeColor = System.Drawing.Color.Gray;
+            customButton3.Location = new System.Drawing.Point(112, 10);
+            customButton3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            customButton3.Name = "customButton3";
+            customButton3.Size = new System.Drawing.Size(156, 33);
+            customButton3.TabIndex = 11;
+            customButton3.Text = "Ongoing";
+            customButton3.TextColor = System.Drawing.Color.Gray;
+            customButton3.UseVisualStyleBackColor = false;
+            // 
+            // customButton4
+            // 
+            customButton4.BackColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            customButton4.BackgroundColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            customButton4.BorderColor = System.Drawing.Color.PaleVioletRed;
+            customButton4.BorderRadius = 5;
+            customButton4.BorderSize = 0;
+            customButton4.FlatAppearance.BorderSize = 0;
+            customButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            customButton4.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline);
+            customButton4.ForeColor = System.Drawing.Color.White;
+            customButton4.Location = new System.Drawing.Point(14, 10);
+            customButton4.Margin = new System.Windows.Forms.Padding(1);
+            customButton4.Name = "customButton4";
+            customButton4.Size = new System.Drawing.Size(93, 33);
+            customButton4.TabIndex = 10;
+            customButton4.Text = "All";
+            customButton4.TextColor = System.Drawing.Color.White;
+            customButton4.UseVisualStyleBackColor = false;
+            // 
+            // customButton5
+            // 
+            customButton5.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            customButton5.BackColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            customButton5.BackgroundColor = System.Drawing.Color.FromArgb(18, 90, 211);
+            customButton5.BorderColor = System.Drawing.Color.White;
+            customButton5.BorderRadius = 10;
+            customButton5.BorderSize = 0;
+            customButton5.Cursor = System.Windows.Forms.Cursors.Hand;
+            customButton5.FlatAppearance.BorderSize = 0;
+            customButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            customButton5.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 238);
+            customButton5.ForeColor = System.Drawing.Color.White;
+            customButton5.Image = (System.Drawing.Image)resources.GetObject("customButton5.Image");
+            customButton5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            customButton5.Location = new System.Drawing.Point(1349, 7);
+            customButton5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            customButton5.Name = "customButton5";
+            customButton5.Padding = new System.Windows.Forms.Padding(10, 0, 23, 0);
+            customButton5.Size = new System.Drawing.Size(214, 55);
+            customButton5.TabIndex = 7;
+            customButton5.Text = "Add Assessment";
+            customButton5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            customButton5.TextColor = System.Drawing.Color.White;
+            customButton5.UseVisualStyleBackColor = false;
+            // 
+            // customButton6
+            // 
+            customButton6.BackColor = System.Drawing.Color.Gainsboro;
+            customButton6.BackgroundColor = System.Drawing.Color.Gainsboro;
+            customButton6.BorderColor = System.Drawing.Color.PaleVioletRed;
+            customButton6.BorderRadius = 5;
+            customButton6.BorderSize = 0;
+            customButton6.FlatAppearance.BorderSize = 0;
+            customButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            customButton6.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline);
+            customButton6.ForeColor = System.Drawing.Color.Gray;
+            customButton6.Location = new System.Drawing.Point(434, 12);
+            customButton6.Margin = new System.Windows.Forms.Padding(1);
+            customButton6.Name = "customButton6";
+            customButton6.Size = new System.Drawing.Size(156, 31);
+            customButton6.TabIndex = 16;
+            customButton6.Text = "Archived";
+            customButton6.TextColor = System.Drawing.Color.Gray;
+            customButton6.UseVisualStyleBackColor = false;
+            // 
             // AssessmentPage
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
@@ -335,6 +515,7 @@
             panelBorder2.ResumeLayout(false);
             panelBorder1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panelBorder3.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
 
@@ -355,5 +536,13 @@
         private OrganizationProfile.CustomButton customButton1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private CustomControls.RJControls.RJTextBox rjTextBox1;
+        private WindowsFormsApp2.CustomButton.PanelBorder panelBorder3;
+        private OrganizationProfile.CustomButton customButton6;
+        private CustomControls.RJControls.RJComboBox rjComboBox1;
+        private OrganizationProfile.CustomButton customButton2;
+        private OrganizationProfile.CustomButton customButton3;
+        private OrganizationProfile.CustomButton customButton4;
+        private OrganizationProfile.CustomButton customButton5;
+        private CustomControls.RJControls.RJComboBox cbSort;
     }
 }
