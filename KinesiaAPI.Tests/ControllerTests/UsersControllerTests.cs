4b6eac7edb6f10a62f1cbd4b85b04a57b8e90325
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-11-09 19:18:54 +0800

    Feat (KinesiaAPI.Tests): Updated UsersControllerTests.cs

--- KinesiaAPI.Tests/ControllerTests/UsersControllerTests.cs
+++ KinesiaAPI.Tests/ControllerTests/UsersControllerTests.cs
@@ -61,5 +61,31 @@ namespace KinesiaAPI.Tests.ControllerTests
             var expectedCount = users.Count(u => u.Status == 0);
             Assert.Equal(expectedCount, returnedUsers.Count());
         }
+
+        [Fact]
+        public async Task GetUsers_ShouldReturnAllUsersWhenTabIsInvalid()
+        {
+            // Arrange
+            var context = TestDbContextFactory.CreateDbContext(nameof(GetUsers_ShouldReturnAllUsersWhenTabIsInvalid));
+
+            var users = DataFactory.GenerateUsers(20);
+            context.Users.AddRange(users);
+            await context.SaveChangesAsync();
+
+            var controller = new UsersController(context);
+
+            // Act
+            var resultNullTab = await controller.GetUsers(currentTab: null);
+            var resultOtherTab = await controller.GetUsers(currentTab: "All");
+
+            // Assert
+            var okResultNull = Assert.IsType<OkObjectResult>(resultNullTab.Result);
+            var returnedUsersNull = Assert.IsAssignableFrom<IEnumerable<DisplayUsersDTO>>(okResultNull.Value);
+            Assert.Equal(20, returnedUsersNull.Count());
+
+            var okResultOther = Assert.IsType<OkObjectResult>(resultOtherTab.Result);
+            var returnedUsersOther = Assert.IsAssignableFrom<IEnumerable<DisplayUsersDTO>>(okResultOther.Value);
+            Assert.Equal(20, returnedUsersOther.Count());
+        }
     }
 }
